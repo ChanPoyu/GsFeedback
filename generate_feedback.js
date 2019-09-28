d48ff2c2573f35b158de6cfc712a61c9d64bd236
chanpoyu <dipsy3333@gmail.com>
2019-09-28 15:09:12 +0900

    add regular expression

--- generate_feedback.js
+++ generate_feedback.js
@@ -1,18 +1,17 @@
 const fs = require('fs')
 
-let path = 'BookList'
+let inputNameFilePath = `namelist.csv`
+let path = '<NEW PATH>'
 let template = `# 良かった点\n- \n# 気になった点\n- \n# その他`
-let numberAndNames = [
-    '03_Ito',
-    '11_Okumura',
-    '20_Saito',
-    '30_Tazaki',
-    '40_Hataketa',
-    '50_Warashina',
-]
 
-if (!fs.existsSync(path)) {
-    fs.mkdirSync(path);
+let fileContent = fs.readFileSync(inputNameFilePath, { encoding: 'utf8' })
+let numberAndNames = fileContent.split(/, | | \n|\n/i)
+
+if (!fs.existsSync(path) && !'<NEW PATH>') {
+    fs.mkdirSync(path)
+}
+else{
+    process.exit()
 }
 
 numberAndNames.forEach((numberAndName) => {
@@ -21,8 +20,7 @@ numberAndNames.forEach((numberAndName) => {
         console.warn(`${fileDirectory} already exist`)
     }else{
         fs.writeFile(fileDirectory, template, (err) => {
-            bool(err) ? console.log(err) : console.log(`Feedback_${numberAndName}.md generated`)
+            Boolean(err) ? console.log(err) : console.log(`Feedback_${numberAndName}.md generated`)
         })
     }
 })
-
